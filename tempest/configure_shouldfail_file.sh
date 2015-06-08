b8242566e4c9ad0f86dbc11ed1b57f2da263153a
Yaroslav Lobankov <ylobankov@mirantis.com>
2015-06-08 14:53:15 +0300

    Adding changes due to using Tempest from Jun 7 2015

--- tempest/configure_shouldfail_file.sh
+++ tempest/configure_shouldfail_file.sh
@@ -37,7 +37,7 @@ add_workaround_for_bug_1427782() {
             cat >> ${SHOULDFAIL_FILE} <<EOF
 
 # Nova
-- tempest.api.compute.images.test_images_oneserver.ImagesOneServerTestJSON.test_create_image_specify_multibyte_character_image_name[gate,id-3b7c6fe4-dfe7-477c-9243-b06359db51e6]:
+- tempest.api.compute.images.test_images_oneserver.ImagesOneServerTestJSON.test_create_image_specify_multibyte_character_image_name[id-3b7c6fe4-dfe7-477c-9243-b06359db51e6]:
     Fail because of https://bugs.launchpad.net/mos/+bug/1427782
 EOF
         fi
