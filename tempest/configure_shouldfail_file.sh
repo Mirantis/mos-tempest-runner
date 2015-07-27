899e72e02e2a700ade225d9ac45382650ae17154
Yaroslav Lobankov <ylobankov@mirantis.com>
2015-07-27 18:09:45 +0300

    Supporting ceph_volumes file to configure "shouldfail" file was added

--- tempest/configure_shouldfail_file.sh
+++ tempest/configure_shouldfail_file.sh
@@ -6,24 +6,32 @@ source ${TOP_DIR}/init_env_variables.sh
 FUEL_RELEASE_DIR="$(echo ${FUEL_RELEASE} | sed "s/\./_/g")"
 SHOULDFAIL_FILE=${DEST}/shouldfail/shouldfail.yaml
 
-add_shouldfail_tests_for_ceph_eph_volumes() {
-    # If Ceph is used as backend for ephemeral volumes, add "shouldfail" tests related to Ceph ephemeral volumes to "shouldfail" file
+add_shouldfail_tests_from_ceph_eph_volumes_file() {
+    # If Ceph is used as backend for ephemeral volumes, add "shouldfail" tests from ceph_eph_volumes file to shouldfail.yaml file
     local is_ceph_ephemeral_volumes="$(ssh ${COMPUTE_HOST} "cat /etc/nova/nova.conf | grep images_type=rbd" 2>/dev/null)"
     if [ "${is_ceph_ephemeral_volumes}" -a -f ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/ceph_eph_volumes ]; then
         cat ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/ceph_eph_volumes >> ${SHOULDFAIL_FILE}
     fi
 }
 
-add_shouldfail_tests_for_cinder_lvm() {
-    # If LVM is used as Cinder backend, add "shouldfail" tests related to LVM to "shouldfail" file
+add_shouldfail_tests_from_ceph_volumes_file() {
+    # If Ceph is used as Cinder backend, add "shouldfail" tests from ceph_volumes file to shouldfail.yaml file
     local volume_driver="$(ssh ${CONTROLLER_HOST} "cat /etc/cinder/cinder.conf | grep volume_driver" 2>/dev/null)"
-    if [ ! "$(echo ${volume_driver} | grep -o RBDDriver)" -a -f ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/cinder_lvm ]; then
-        cat ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/cinder_lvm >> ${SHOULDFAIL_FILE}
+    if [ "$(echo ${volume_driver} | grep -o RBDDriver)" -a -f ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/ceph_volumes ]; then
+        cat ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/ceph_volumes >> ${SHOULDFAIL_FILE}
     fi
 }
 
-add_shouldfail_tests_for_swift() {
-    # If Swift is deployed, add "shouldfail" tests related to Swift to "shouldfail" file
+add_shouldfail_tests_from_lvm_volumes_file() {
+    # If LVM is used as Cinder backend, add "shouldfail" tests from lvm_volumes file to shouldfail.yaml file
+    local volume_driver="$(ssh ${CONTROLLER_HOST} "cat /etc/cinder/cinder.conf | grep volume_driver" 2>/dev/null)"
+    if [ ! "$(echo ${volume_driver} | grep -o RBDDriver)" -a -f ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/lvm_volumes ]; then
+        cat ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/lvm_volumes >> ${SHOULDFAIL_FILE}
+    fi
+}
+
+add_shouldfail_tests_from_swift_file() {
+    # If Swift is deployed, add "shouldfail" tests from swift file to shouldfail.yaml file
     local is_swift="$(ssh ${CONTROLLER_HOST} "if [ -d /etc/swift ]; then echo true; else echo false; fi")"
     if [ "${is_swift}" = "true" -a -f ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/swift ]; then
         cat ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/swift >> ${SHOULDFAIL_FILE}
@@ -37,9 +45,10 @@ choose_and_configure_shouldfail_file() {
         if [ -f ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/shouldfail.yaml ]; then
             cp ${DEST}/shouldfail/${FUEL_RELEASE_DIR}/shouldfail.yaml ${SHOULDFAIL_FILE}
 
-            add_shouldfail_tests_for_ceph_eph_volumes
-            add_shouldfail_tests_for_cinder_lvm
-            add_shouldfail_tests_for_swift
+            add_shouldfail_tests_from_ceph_eph_volumes_file
+            add_shouldfail_tests_from_ceph_volumes_file
+            add_shouldfail_tests_from_lvm_volumes_file
+            add_shouldfail_tests_from_swift_file
         else
             # Use default "shouldfail" file
             cp ${DEST}/shouldfail/default_shouldfail.yaml ${SHOULDFAIL_FILE}
