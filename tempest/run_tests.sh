4334e1527551452af0b346277be694ef7bf44d1b
Yaroslav Lobankov <ylobankov@mirantis.com>
2015-08-20 17:11:03 +0300

    Always show actual Tempest commit ID

--- tempest/run_tests.sh
+++ tempest/run_tests.sh
@@ -70,12 +70,16 @@ collect_results() {
 
 run() {
     message "Running Tempest tests"
-    message "Tempest commit ID is ${TEMPEST_COMMIT_ID}"
+
     cd ${DEST}/tempest
+    message "Tempest commit ID is $(git log -n 1 | awk '/commit/ {print $2}')"
+
     configure_tempest
     configure_shouldfail_file
+
     run_tests
     collect_results
+
     cd ${TOP_DIR}
 }
 
